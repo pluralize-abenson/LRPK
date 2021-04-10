943dd5fb3581920bff4ee191fed5813699f046aa
Alexander Benson <65480364+pluralize-abenson@users.noreply.github.com>
2021-04-09 20:33:15 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,94 @@
+# main.py, for install verification
+
+# MME565 LRPK E2.9
+
+import MME565
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.collections import PatchCollection
+
+# define polygon
+polygon_vertices = [
+    [18, 11],
+    [25, 13.5],
+    [20, 14],
+    [21, 17],
+    [17, 15],
+    [11, 17],
+    [15, 13],
+    [13, 12],
+    ]
+
+# define free workspace
+free_workspace_vertices = [
+    [7, 8],
+    [30, 8],
+    [30, 20],
+    [7, 20]
+]
+
+polygon = MME565.Polygon(polygon_vertices)
+free_workspace = MME565.Polygon(free_workspace_vertices)
+
+# conduct trapezoidation over free workspace and polygon
+trapezoids = MME565.trapezoidation(free_workspace, [polygon])
+
+# plot convex / non-convex vertices
+fig, ax = plt.subplots()
+patches = [mpatches.Polygon(polygon.vertex_array), mpatches.Polygon(free_workspace.vertex_array)]
+colors = np.linspace(0, 1, 9)
+collection = PatchCollection(patches, cmap=plt.cm.hsv, alpha=0.3)
+collection.set_array(colors)
+ax.add_collection(collection)
+for vertex in polygon.vertices:
+    if vertex.convex:
+        ax.annotate("convex", (vertex.x, vertex.y))
+    else:
+        ax.annotate("non-convex", (vertex.x, vertex.y))
+plt.axis('equal')
+plt.tight_layout()
+
+# plot vertex coordinates
+fig, ax = plt.subplots()
+patches = [mpatches.Polygon(polygon.vertex_array), mpatches.Polygon(free_workspace.vertex_array)]
+colors = np.linspace(0, 1, 9)
+collection = PatchCollection(patches, cmap=plt.cm.hsv, alpha=0.3)
+collection.set_array(colors)
+ax.add_collection(collection)
+for vertex in polygon.vertices:
+    ax.annotate(vertex.cartesian, (vertex.x, vertex.y))
+for vertex in free_workspace.vertices:
+    ax.annotate(vertex.cartesian, (vertex.x, vertex.y))
+plt.axis('equal')
+plt.tight_layout()
+
+# plot vertex type
+fig, ax = plt.subplots()
+patches = [mpatches.Polygon(polygon.vertex_array), mpatches.Polygon(free_workspace.vertex_array)]
+colors = np.linspace(0, 1, 9)
+collection = PatchCollection(patches, cmap=plt.cm.hsv, alpha=0.3)
+collection.set_array(colors)
+ax.add_collection(collection)
+for vertex in polygon.vertices:
+    ax.annotate(vertex.type, (vertex.x, vertex.y))
+plt.axis('equal')
+plt.tight_layout()
+
+# plot trapezoidation
+fig, ax = plt.subplots()
+patches = [
+    mpatches.Polygon(polygon.vertex_array),
+    mpatches.Polygon(free_workspace.vertex_array),
+    ]
+for trapezoid in trapezoids:
+    patches.append(mpatches.Polygon(trapezoid.vertex_array))
+colors = np.linspace(0, 1, 9)
+collection = PatchCollection(patches, cmap=plt.cm.hsv, alpha=0.3)
+collection.set_array(colors)
+ax.add_collection(collection)
+for i, trapezoid in enumerate(trapezoids):
+    ax.annotate(f"T{i}", trapezoid.center_cartesian)
+plt.axis('equal')
+plt.tight_layout()
+plt.show()
